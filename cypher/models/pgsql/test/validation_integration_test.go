e9ace7f4c8117314ef881440cf69af9d437d5d22
John Hopper <jhopper@specterops.io>
2025-06-16 14:01:36 -0700

    fix (test): fixup live translation validation for cysql

--- cypher/models/pgsql/test/validation_integration_test.go
+++ cypher/models/pgsql/test/validation_integration_test.go
@@ -2,78 +2,89 @@
 
 package test
 
-//import (
-//	"context"
-//	"fmt"
-//	"runtime/debug"
-//	"testing"
-//
-//	"github.com/specterops/dawgs"
-//	"github.com/specterops/dawgs/drivers/pg"
-//	"github.com/specterops/dawgs/graph"
-//	"github.com/specterops/dawgs/util/size"
-//	schema "github.com/specterops/bloodhound/graphschema"
-//	"github.com/specterops/bloodhound/src/test/integration/utils"
-//	"github.com/stretchr/testify/require"
-//)
-//
-//func TestTranslationTestCases(t *testing.T) {
-//	var (
-//		textCtx, done = context.WithCancel(context.Background())
-//		cfg, err      = utils.LoadIntegrationTestConfig()
-//	)
-//
-//	defer done()
-//
-//	require.Nil(t, err)
-//
-//	if connection, err := dawgs.Open(context.TODO(), pg.DriverName, dawgs.Config{
-//		GraphQueryMemoryLimit: size.Gibibyte,
-//		DriverCfg:             cfg.Database.PostgreSQLConnectionString(),
-//	}); err != nil {
-//		t.Fatalf("Failed opening database connection: %v", err)
-//	} else if pgConnection, typeOK := connection.(*pg.Driver); !typeOK {
-//		t.Fatalf("Invalid connection type: %T", connection)
-//	} else {
-//		defer connection.Close(textCtx)
-//
-//		graphSchema := schema.DefaultGraphSchema()
-//
-//		graphSchema.Graphs[0].Nodes = graphSchema.DefaultGraph.Nodes.Add(
-//			graph.StringKind("NodeKind1"),
-//			graph.StringKind("NodeKind2"),
-//		)
-//
-//		graphSchema.Graphs[0].Edges = graphSchema.DefaultGraph.Edges.Add(
-//			graph.StringKind("EdgeKind1"),
-//			graph.StringKind("EdgeKind2"),
-//		)
-//
-//		if err := connection.AssertSchema(textCtx, graphSchema); err != nil {
-//			t.Fatalf("Failed asserting graph schema: %v", err)
-//		}
-//
-//		casesRun := 0
-//
-//		if testCases, err := ReadTranslationTestCases(); err != nil {
-//			t.Fatal(err)
-//		} else {
-//			for _, testCase := range testCases {
-//				t.Run(testCase.Name, func(t *testing.T) {
-//					defer func() {
-//						if err := recover(); err != nil {
-//							debug.PrintStack()
-//							t.Error(err)
-//						}
-//					}()
-//
-//					testCase.AssertLive(textCtx, t, pgConnection)
-//				})
-//
-//				casesRun += 1
-//			}
-//		}
-//
-//		fmt.Printf("Validated %d test cases\n", casesRun)
-//	}
-//}
+import (
+	"context"
+	"fmt"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"os"
+	"runtime/debug"
+	"testing"
+
+	"github.com/specterops/dawgs"
+	"github.com/specterops/dawgs/drivers/pg"
+	"github.com/specterops/dawgs/graph"
+	"github.com/specterops/dawgs/util/size"
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	PGConnectionStringEV = "PG_CONNECTION_STRING"
+)
+
+func TestTranslationTestCases(t *testing.T) {
+	var (
+		testCtx, done   = context.WithCancel(context.Background())
+		pgConnectionStr = os.Getenv(PGConnectionStringEV)
+	)
+
+	defer done()
+
+	require.NotEmpty(t, pgConnectionStr)
+
+	if pgxPool, err := pgxpool.New(testCtx, pgConnectionStr); err != nil {
+		t.Fatalf("Failed opening database connection: %v", err)
+	} else if connection, err := dawgs.Open(context.TODO(), pg.DriverName, dawgs.Config{
+		GraphQueryMemoryLimit: size.Gibibyte,
+		Pool:                  pgxPool,
+	}); err != nil {
+		t.Fatalf("Failed opening database connection: %v", err)
+	} else if pgConnection, typeOK := connection.(*pg.Driver); !typeOK {
+		t.Fatalf("Invalid connection type: %T", connection)
+	} else {
+		defer connection.Close(testCtx)
+
+		graphSchema := graph.Schema{
+			Graphs: []graph.Graph{{
+				Name: "test",
+				Nodes: graph.Kinds{
+					graph.StringKind("NodeKind1"),
+					graph.StringKind("NodeKind2"),
+				},
+				Edges: graph.Kinds{
+					graph.StringKind("EdgeKind1"),
+					graph.StringKind("EdgeKind2"),
+				},
+			}},
+			DefaultGraph: graph.Graph{
+				Name: "test",
+			},
+		}
+
+		if err := connection.AssertSchema(testCtx, graphSchema); err != nil {
+			t.Fatalf("Failed asserting graph schema: %v", err)
+		}
+
+		casesRun := 0
+
+		if testCases, err := ReadTranslationTestCases(); err != nil {
+			t.Fatal(err)
+		} else {
+			for _, testCase := range testCases {
+				t.Run(testCase.Name, func(t *testing.T) {
+					defer func() {
+						if err := recover(); err != nil {
+							debug.PrintStack()
+							t.Error(err)
+						}
+					}()
+
+					testCase.AssertLive(testCtx, t, pgConnection)
+				})
+
+				casesRun += 1
+			}
+		}
+
+		fmt.Printf("Validated %d test cases\n", casesRun)
+	}
+}
