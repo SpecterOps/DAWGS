cbe79b2306987ccd3872d9704cee59d3b0053d2a
Brandon Shearin <bshearin@specterops.io>
2025-08-06 14:28:07 -0700

    remove initial impl

--- drivers/pg/changestream/daemon.go
+++ drivers/pg/changestream/daemon.go
@@ -17,13 +17,10 @@ import (
 
 const (
 	// Limit batch sizes
-	BATCH_SIZE = 1_000
+	batchSize = 1_000
 )
 
 var (
-	// todo: why are the following properties safe to always ignore when computing hashes?
-	// todo: the common, ad, and azure packages where these string constants are defined are
-	// in ce repo. whats a good way to pull these constants in to dawgs?
 	ignoredPropertiesKeys = map[string]struct{}{
 		// common.ObjectID.String():      {},
 		// common.LastSeen.String():      {},
@@ -35,79 +32,256 @@ var (
 	}
 )
 
-type Log interface {
-	ResolveNodeChangeStatus(ctx context.Context, proposedChange *NodeChange) (ChangeStatus, error)
-	EvaluateNodeChange(proposedChange *NodeChange) (ChangeStatus, error)
-	LastEdgeChange(ctx context.Context, proposedChange *EdgeChange) (ChangeStatus, error)
-	CachedLastEdgeChange(proposedChange *EdgeChange) (ChangeStatus, error)
-	Submit(ctx context.Context, change Change) bool
+// todo: use golang-LRU cache
+// time multiple ingest runs with no cache, size 1_000, 100_000
+type ChangeCache struct {
+	data  map[string]ChangeStatus
+	mutex *sync.RWMutex
 }
 
-type Daemon struct {
-	// ingest writes to writerC.
-	writerC chan<- Change
-	// we continuously read changes from readerC to insert into pg
-	readerC <-chan Change
-
-	pgxPool          *pgxpool.Pool
-	kindMapper       pg.KindMapper
-	changeCacheLock  *sync.RWMutex
-	changeCache      map[string]ChangeStatus
-	State            *stateManager // todo: can we make this private?
-	nodeChangeBuffer []*NodeChange
-	edgeChangeBuffer []*EdgeChange
-}
-
-func NewDaemon(ctx context.Context, flags GetFlagByKeyer, pgxPool *pgxpool.Pool, kindMapper pg.KindMapper) *Daemon {
-	return &Daemon{
-		changeCache:     make(map[string]ChangeStatus),
-		changeCacheLock: &sync.RWMutex{},
-		State:           newStateManager(flags),
-		pgxPool:         pgxPool,
-		kindMapper:      kindMapper,
+func newChangeCache() ChangeCache {
+	return ChangeCache{
+		data:  make(map[string]ChangeStatus),
+		mutex: &sync.RWMutex{},
 	}
 }
 
-func (s *Daemon) PGX() *pgxpool.Pool {
-	return s.pgxPool
+func (s *ChangeCache) get(key string) (ChangeStatus, bool) {
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
+	val, ok := s.data[key]
+	return val, ok
 }
 
-func (s *Daemon) EvaluateNodeChange(proposedChange *NodeChange) (ChangeStatus, error) {
+func (s *ChangeCache) put(key string, value ChangeStatus) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	s.data[key] = value
+}
+
+func (s *ChangeCache) evaluateNodeChange(proposedChange *NodeChange) (ChangeStatus, error) {
 	var (
-		lastChange  ChangeStatus
+		status      ChangeStatus
 		identityKey = proposedChange.IdentityKey()
 	)
 
 	if propertiesHash, err := proposedChange.Properties.Hash(ignoredPropertiesKeys); err != nil {
-		return lastChange, err
+		return status, err
+	} else if kindsHash, err := proposedChange.Kinds.Hash(); err != nil {
+		return status, err
 	} else {
 		// Track the properties hash and kind IDs
-		lastChange.PropertiesHash = propertiesHash
+		combinedHash := append(propertiesHash, kindsHash...)
+		status.PropertiesHash = combinedHash
 	}
 
-	if cachedChange, hasCachedChange := s.LastCachedChange(identityKey); hasCachedChange {
-		lastChange.Changed = !bytes.Equal(lastChange.PropertiesHash, cachedChange.PropertiesHash)
-		lastChange.Exists = true
+	if cachedChange, ok := s.get(identityKey); ok {
+		status.Changed = !bytes.Equal(status.PropertiesHash, cachedChange.PropertiesHash)
+		status.Exists = true
 	} else {
-		// If the change log is disabled then mark every non-cached lookup as changed
-		lastChange.Changed = !s.State.isEnabled()
+		// mark every non-cached lookup as changed
+		status.Changed = true
 	}
 
 	// Ensure this makes it into the cache before returning
-	s.PutCachedChange(identityKey, lastChange)
-	return lastChange, nil
+	s.put(identityKey, status)
+	return status, nil
 }
 
-func (s *Daemon) ResolveNodeChangeStatus(ctx context.Context, proposedChange *NodeChange) (ChangeStatus, error) {
-	lastChange, err := s.EvaluateNodeChange(proposedChange)
+type ChangeWriter struct {
+	PGX        *pgxpool.Pool
+	KindMapper pg.KindMapper
+}
+
+func newChangeWriter(pgxPool *pgxpool.Pool, kindMapper pg.KindMapper) ChangeWriter {
+	return ChangeWriter{
+		PGX:        pgxPool,
+		KindMapper: kindMapper,
+	}
+}
+
+func (s *ChangeWriter) flushNodeChanges(ctx context.Context, changes []*NodeChange) error {
+	// Early exit check for empty buffer flushes
+	if len(changes) == 0 {
+		return nil
+	}
+
+	var (
+		numChanges  = len(changes)
+		now         = time.Now()
+		copyColumns = []string{
+			"node_id",
+			"kind_ids",
+			"hash",
+			"change_type",
+			"created_at",
+		}
+	)
+
+	iterator := func(i int) ([]any, error) {
+		c := changes[i]
+
+		if mappedKindIDs, err := s.KindMapper.MapKinds(ctx, c.Kinds); err != nil {
+			return nil, fmt.Errorf("node kind ID mapping error: %w", err)
+		} else if hash, err := c.Hash(); err != nil {
+			return nil, err
+		} else {
+			rows := []any{
+				c.NodeID,
+				mappedKindIDs,
+				hash,
+				c.Type(),
+				now,
+			}
+
+			return rows, nil
+		}
+	}
+
+	if _, err := s.PGX.CopyFrom(ctx, pgx.Identifier{"node_change_stream"}, copyColumns, pgx.CopyFromSlice(numChanges, iterator)); err != nil {
+		return fmt.Errorf("change stream node change insert error: %v", err)
+	} else {
+		return nil
+	}
+}
+
+type loop struct {
+	State         *stateManager
+	ReaderC       <-chan Change
+	WriterC       chan<- Change
+	ChangeWriter  ChangeWriter
+	Cache         ChangeCache
+	FlushInterval time.Duration
+	NodeBuffer    []*NodeChange
+	BatchSize     int
+}
+
+func newLoop(ctx context.Context, state *stateManager, writer ChangeWriter, cache ChangeCache, batchSize int) loop {
+	writerC, readerC := channels.BufferedPipe[Change](ctx)
+
+	return loop{
+		State:         state,
+		ReaderC:       readerC,
+		WriterC:       writerC,
+		ChangeWriter:  writer,
+		Cache:         cache,
+		FlushInterval: 5 * time.Second,
+		NodeBuffer:    make([]*NodeChange, 0),
+		BatchSize:     batchSize,
+	}
+}
+
+func (s *loop) start(ctx context.Context) error {
+	ticker := time.NewTicker(s.FlushInterval)
+
+	defer func() {
+		close(s.WriterC)
+		ticker.Stop()
+		slog.InfoContext(ctx, "Shutting down change stream")
+	}()
+
+	// initialize the node_change_stream, edge_change_stream tables
+	if _, err := s.ChangeWriter.PGX.Exec(ctx, ASSERT_TABLE_SQL); err != nil {
+		return fmt.Errorf("failed asserting changelog tablespace: %w", err)
+	}
+
+	slog.InfoContext(ctx, "Starting change stream")
+
+	lastNodeWatermark := 0
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+
+			// todo: the following two cases are missing some logic around feature flag checking and watermark logic
+		case change := <-s.ReaderC:
+			if !s.State.isEnabled() {
+				continue
+			}
+
+			switch typed := change.(type) {
+			case *NodeChange:
+				s.NodeBuffer = append(s.NodeBuffer, typed)
+				if len(s.NodeBuffer) >= s.BatchSize {
+					// todo: error handling on flush?
+					if err := s.ChangeWriter.flushNodeChanges(ctx, s.NodeBuffer); err != nil {
+						slog.Warn(err.Error())
+					}
+					s.NodeBuffer = s.NodeBuffer[:0]
+				}
+			}
+
+		case <-ticker.C:
+			lastNodeWatermark = s.maybeFlush(ctx, lastNodeWatermark)
+		}
+	}
+}
+
+// maybeFlush checks if the node buffer has remained the same size over two ticks.
+// This implies inactivity—no new changes—and triggers a flush of any remaining changes.
+// The function returns the new watermark for tracking buffer growth across ticks.
+func (s *loop) maybeFlush(ctx context.Context, lastNodeWatermark int) int {
+	numNodeChanges := len(s.NodeBuffer)
+
+	hasNodeChangesToFlush := numNodeChanges > 0 && numNodeChanges == lastNodeWatermark
+
+	if !hasNodeChangesToFlush { // &&!hasEdgeChangesToFlush
+		// No eligible flush needed
+		lastNodeWatermark = numNodeChanges
+		return lastNodeWatermark
+	}
+
+	if hasNodeChangesToFlush {
+		if err := s.ChangeWriter.flushNodeChanges(ctx, s.NodeBuffer); err != nil {
+			slog.Warn(err.Error())
+		}
+		s.NodeBuffer = s.NodeBuffer[:0]
+	}
+
+	lastNodeWatermark = len(s.NodeBuffer)
+
+	return lastNodeWatermark
+}
+
+// todo: ChangeLog is the public export for this package to be consumed by bloodhound.
+type Changelog struct {
+	Cache  ChangeCache
+	Writer ChangeWriter
+	Loop   loop
+}
+
+func NewChangelogDaemon(ctx context.Context, flags GetFlagByKeyer, pgxPool *pgxpool.Pool, kindMapper pg.KindMapper, batchSize int) *Changelog {
+	cache := newChangeCache()
+	writer := newChangeWriter(pgxPool, kindMapper)
+	state := newStateManager(flags)
+	loop := newLoop(ctx, state, writer, cache, batchSize)
+
+	// prime the feature flag upon initalization
+	// because state is not a pointer value this doesn't actually update state
+	// so i changed it to a pointer
+	state.CheckFeatureFlag(ctx)
+
+	go loop.start(ctx)
+
+	return &Changelog{
+		Cache:  cache,
+		Writer: writer,
+		Loop:   loop,
+	}
+}
+
+func (s *Changelog) ResolveNodeChangeStatus(ctx context.Context, proposedChange *NodeChange) (ChangeStatus, error) {
+	lastChange, err := s.Cache.evaluateNodeChange(proposedChange)
 
 	if err != nil || lastChange.Exists {
 		return lastChange, err
 	}
 
-	if s.State.isEnabled() {
+	// todo: move state should be top level so no gross nesting
+	if s.Loop.State.isEnabled() {
 		var (
-			lastChangeRow = s.pgxPool.QueryRow(ctx, LAST_NODE_CHANGE_SQL, proposedChange.NodeID, lastChange.PropertiesHash)
+			lastChangeRow = s.Writer.PGX.QueryRow(ctx, LAST_NODE_CHANGE_SQL, proposedChange.NodeID, lastChange.PropertiesHash)
 			err           = lastChangeRow.Scan(&lastChange.Changed, &lastChange.Type)
 		)
 
@@ -125,191 +299,13 @@ func (s *Daemon) ResolveNodeChangeStatus(ctx context.Context, proposedChange *No
 		}
 
 		// Ensure this makes it into the cache before returning
-		s.PutCachedChange(proposedChange.IdentityKey(), lastChange)
+		s.Cache.put(proposedChange.IdentityKey(), lastChange)
 	}
 
 	return lastChange, nil
+
 }
 
-func (s *Daemon) Submit(ctx context.Context, change Change) bool {
-	return channels.Submit(ctx, s.writerC, change)
-}
-
-func (s *Daemon) LastCachedChange(cacheKey string) (ChangeStatus, bool) {
-	s.changeCacheLock.RLock()
-	defer s.changeCacheLock.RUnlock()
-
-	cachedChange, hasCachedChange := s.changeCache[cacheKey]
-	return cachedChange, hasCachedChange
-}
-
-func (s *Daemon) PutCachedChange(cacheKey string, cachedLookup ChangeStatus) {
-	s.changeCacheLock.Lock()
-	defer s.changeCacheLock.Unlock()
-
-	s.changeCache[cacheKey] = cachedLookup
-}
-
-func (s *Daemon) RunLoop(ctx context.Context) error {
-	// initialize the node_change_stream, edge_change_stream tables
-	if _, err := s.pgxPool.Exec(ctx, ASSERT_TABLE_SQL); err != nil {
-		return fmt.Errorf("failed asserting changelog tablespace: %w", err)
-	}
-
-	// todo: return to partitioning once initial implementation is working on one table
-	// if err := AssertChangelogPartition(ctx, s.pgxPool); err != nil {
-	// 	return fmt.Errorf("failed asserting changelog partition: %w", err)
-	// }
-
-	go s.runLoopBody(ctx, 5*time.Second)
-
-	return nil
-}
-
-func (s *Daemon) runLoopBody(ctx context.Context, flushInterval time.Duration) {
-	ticker := time.NewTicker(flushInterval)
-	defer func() {
-		s.pgxPool.Close()
-		close(s.writerC)
-		ticker.Stop()
-		slog.InfoContext(ctx, "Shutting down change stream")
-	}()
-
-	slog.InfoContext(ctx, "Starting change stream")
-
-	lastNodeWatermark := 0
-	// lastEdgeWatermark := 0
-
-	for {
-		now := time.Now()
-
-		select {
-		case <-ctx.Done():
-			return
-
-		case change := <-s.readerC:
-			s.handleIncomingChange(ctx, change, now)
-
-		case <-ticker.C:
-			lastNodeWatermark = s.maybeFlush(ctx, lastNodeWatermark)
-		}
-	}
-}
-
-func (s *Daemon) handleIncomingChange(ctx context.Context, change Change, now time.Time) {
-	if err := s.State.CheckFeatureFlag(ctx); err != nil {
-		slog.ErrorContext(ctx, fmt.Sprintf("feature flag check failed: %v", err))
-		return
-	}
-
-	if !s.State.isEnabled() {
-		return
-	}
-
-	// if err := s.State.checkChangelogPartitions(ctx, now, s.pgxPool); err != nil {
-	// 	slog.ErrorContext(ctx, fmt.Sprintf("partition check failed: %v", err))
-	// 	return
-	// }
-
-	s.QueueChange(ctx, now, change)
-}
-
-func (s *Daemon) QueueChange(ctx context.Context, now time.Time, nextChange Change) {
-	switch typedChange := nextChange.(type) {
-	case *NodeChange:
-		s.nodeChangeBuffer = append(s.nodeChangeBuffer, typedChange)
-
-		if len(s.nodeChangeBuffer) >= BATCH_SIZE {
-			if err := s.FlushNodeChanges(ctx); err != nil {
-				slog.ErrorContext(ctx, fmt.Sprintf("writing changes to change stream failed: %v", err))
-			}
-		}
-
-	case *EdgeChange:
-
-	default:
-		slog.ErrorContext(ctx, fmt.Sprintf("Unexpected change type: %T", nextChange))
-	}
-}
-
-func (s *Daemon) FlushNodeChanges(ctx context.Context) error {
-	// Early exit check for empty buffer flushes
-	if len(s.nodeChangeBuffer) == 0 {
-		return nil
-	}
-
-	var (
-		numChanges  = len(s.nodeChangeBuffer)
-		now         = time.Now()
-		copyColumns = []string{
-			"node_id",
-			"kind_ids",
-			"properties_hash",
-			"property_fields",
-			"change_type",
-			"created_at",
-		}
-
-		iteratorFunc = func(idx int) ([]any, error) {
-			nextNodeChange := s.nodeChangeBuffer[idx]
-
-			if mappedKindIDs, err := s.kindMapper.MapKinds(ctx, nextNodeChange.Kinds); err != nil {
-				return nil, fmt.Errorf("node kind ID mapping error: %w", err)
-			} else if propertiesHash, err := nextNodeChange.Properties.Hash(ignoredPropertiesKeys); err != nil {
-				return nil, fmt.Errorf("node properties hash error: %w", err)
-			} else {
-				rows := []any{
-					nextNodeChange.NodeID,
-					mappedKindIDs,
-					propertiesHash,
-					nextNodeChange.Properties.Keys(nil),
-					nextNodeChange.Type(),
-					now,
-				}
-
-				return rows, nil
-			}
-		}
-	)
-
-	slog.DebugContext(ctx, fmt.Sprintf("flushing %d node changes", numChanges))
-
-	// pgx.Identifier{partitionName}
-	if _, err := s.pgxPool.CopyFrom(ctx, pgx.Identifier{"node_change_stream"}, copyColumns, pgx.CopyFromSlice(numChanges, iteratorFunc)); err != nil {
-		slog.Info(fmt.Sprintf("change stream node change insert error: %v", err))
-	}
-
-	// TODO: Should the buffer clear be dependent on no errors?
-	s.nodeChangeBuffer = s.nodeChangeBuffer[:0]
-	return nil
-}
-
-// maybeFlush will only flush changes when the buffer has not increased in size in two ticks.
-// if the buffer is the same size that implies no new changes have arrived
-func (s *Daemon) maybeFlush(ctx context.Context, lastNodeWatermark int) int {
-	numNodeChanges := len(s.nodeChangeBuffer)
-
-	hasNodeChangesToFlush := numNodeChanges > 0 && numNodeChanges == lastNodeWatermark
-
-	if !hasNodeChangesToFlush { // &&!hasEdgeChangesToFlush
-		// No eligible flush needed
-		lastNodeWatermark = numNodeChanges
-		return lastNodeWatermark
-	}
-
-	// todo: add partitioning l8r
-	// if err := s.State.checkChangelogPartitions(ctx, now, s.pgxPool); err != nil {
-	// 	slog.ErrorContext(ctx, fmt.Sprintf("change log state check error: %v", err))
-	// 	return
-	// }
-
-	if hasNodeChangesToFlush {
-		if err := s.FlushNodeChanges(ctx); err != nil {
-			slog.ErrorContext(ctx, fmt.Sprintf("writing node changes to change stream failed: %v", err))
-		}
-	}
-
-	lastNodeWatermark = len(s.nodeChangeBuffer)
-
-	return lastNodeWatermark
+func (s *Changelog) Submit(ctx context.Context, change Change) bool {
+	return channels.Submit(ctx, s.Loop.WriterC, change)
 }
--- drivers/pg/changestream/daemon_integration_test.go
+++ drivers/pg/changestream/daemon_integration_test.go
@@ -27,72 +27,7 @@ func (s *flagDisabled) GetFlagByKey(ctx context.Context, flag string) (bool, err
 	return false, nil
 }
 
-// TODO: use pgtestdb
-func setupIntegrationTest(t *testing.T, enableChangelog bool) (*changestream.Daemon, context.Context, func()) {
-	t.Helper()
-
-	ctx, cancel := context.WithCancel(context.Background())
-	pgxPool, err := pgxpool.New(ctx, pg_connection_string)
-	require.NoError(t, err)
-
-	_, err = dawgs.Open(ctx, pg.DriverName, dawgs.Config{
-		ConnectionString: pg_connection_string,
-		Pool:             pgxPool,
-	})
-	require.NoError(t, err)
-
-	// kindMapper, err := pg.KindMapperFromGraphDatabase(graphDB)
-	// require.NoError(t, err)
-
-	// initialize a graph with bare minimum kinds... parent bloodhound app has
-	// a defaultGraphSchema defined in the graphschema package. we don't have that nicety available to us here.
-	graphSchema := graph.Schema{
-		Graphs: []graph.Graph{{
-			Name: "test",
-			Nodes: graph.Kinds{
-				graph.StringKind("NodeKind1"),
-				graph.StringKind("NodeKind2"),
-			},
-			Edges: graph.Kinds{
-				graph.StringKind("EdgeKind1"),
-				graph.StringKind("EdgeKind2"),
-			},
-		}},
-		DefaultGraph: graph.Graph{
-			Name: "test",
-		},
-	}
-	schemaManager := pg.NewSchemaManager(pgxPool)
-	err = schemaManager.AssertSchema(ctx, graphSchema)
-	require.NoError(t, err)
-
-	_, err = pgxPool.Exec(ctx, changestream.ASSERT_TABLE_SQL)
-	require.NoError(t, err)
-
-	// err = changestream.AssertChangelogPartition(ctx, pgxPool)
-	// require.NoError(t, err)
-
-	var flag changestream.GetFlagByKeyer
-	if enableChangelog {
-		flag = &flagEnabled{}
-	} else {
-		flag = &flagDisabled{}
-	}
-
-	daemon := changestream.NewDaemon(ctx, flag, pgxPool, schemaManager)
-	daemon.State.CheckFeatureFlag(ctx) // todo: this sets the enabled flag on the state...
-
-	return daemon, ctx, func() {
-		_, err := pgxPool.Exec(ctx, "TRUNCATE node_change_stream")
-		if err != nil {
-			t.Logf("warning: cleanup failed: %v", err)
-		}
-		pgxPool.Close()
-		cancel()
-	}
-}
-
-func setupIntegrationTestRefactor(t *testing.T, enableChangelog bool) (*changestream.Changelog, context.Context, func()) {
+func setupIntegrationTest(t *testing.T, enableChangelog bool) (*changestream.Changelog, context.Context, func()) {
 	t.Helper()
 
 	ctx, cancel := context.WithCancel(context.Background())
@@ -152,156 +87,10 @@ func setupIntegrationTestRefactor(t *testing.T, enableChangelog bool) (*changest
 		cancel()
 	}
 }
-func TestResolveNodeChangeStatus(t *testing.T) {
-	t.Run("cache hit, return early", func(t *testing.T) {
-		daemon, ctx, teardown := setupIntegrationTest(t, true)
-		defer teardown()
-
-		props := graph.NewProperties().SetAll(map[string]any{"a": 1, "b": 2})
-		hashedProps, _ := props.Hash(nil)
-		proposedChange := changestream.NewNodeChange(changestream.ChangeTypeModified, "abc", []graph.Kind{graph.StringKind("kind A")}, props)
-
-		// cache control. simulate a cache hit
-		daemon.PutCachedChange(proposedChange.IdentityKey(), changestream.ChangeStatus{
-			PropertiesHash: hashedProps,
-		})
-
-		status, err := daemon.ResolveNodeChangeStatus(ctx, proposedChange)
-		require.NoError(t, err)
-		require.False(t, status.Changed)
-		require.True(t, status.Exists)
-	})
-
-	t.Run("cache miss, changelog disabled", func(t *testing.T) {
-		daemon, ctx, teardown := setupIntegrationTest(t, false)
-		defer teardown()
-
-		// simulate turning off change log
-		// daemon.State.DisableDaemon()
-
-		props := graph.NewProperties().SetAll(map[string]any{"a": 1, "b": 2})
-		proposedChange := changestream.NewNodeChange(changestream.ChangeTypeModified, "abc", []graph.Kind{graph.StringKind("kind A")}, props)
-
-		status, err := daemon.ResolveNodeChangeStatus(ctx, proposedChange)
-		require.NoError(t, err)
-		require.True(t, status.Changed)
-		require.False(t, status.Exists)
-	})
-
-	t.Run("cache miss, changelog enabled, DB match with same hash", func(t *testing.T) {
-		daemon, ctx, teardown := setupIntegrationTest(t, true)
-		defer teardown()
-
-		props := graph.NewProperties().SetAll(map[string]any{"foo": "bar", "a": 1})
-		hash, _ := props.Hash(nil)
-		nodeID := "node-match-same"
-
-		// simulate an existing db record with same property hash
-		_, err := daemon.PGX().Exec(ctx, `
-			INSERT INTO node_change_stream (node_id, kind_ids, hash, property_fields, change_type, created_at)
-			VALUES ($1, '{}', $2, '{}', $3, now())
-		`, nodeID, hash, changestream.ChangeTypeModified)
-		require.NoError(t, err)
-
-		proposedChange := changestream.NewNodeChange(changestream.ChangeTypeModified, nodeID, graph.Kinds{}, props)
-
-		status, err := daemon.ResolveNodeChangeStatus(ctx, proposedChange)
-		require.NoError(t, err)
-		require.False(t, status.Changed)
-		require.True(t, status.Exists)
-	})
-
-	t.Run("cache miss, changelog enabled, DB match with different hash", func(t *testing.T) {
-		daemon, ctx, teardown := setupIntegrationTest(t, true)
-		defer teardown()
-
-		props := graph.NewProperties().SetAll(map[string]any{"foo": "bar", "a": 1})
-		nodeID := "node-match-same"
-
-		existingProps := graph.NewProperties().SetAll(map[string]any{"foo": "bar"})
-		existingPropsHash, _ := existingProps.Hash(nil)
-
-		// simulate an existing db record with differenty property hash
-		_, err := daemon.PGX().Exec(ctx, `
-			INSERT INTO node_change_stream (node_id, kind_ids, hash, property_fields, change_type, created_at)
-			VALUES ($1, '{}', $2, '{}', $3, now())
-		`, nodeID, existingPropsHash, changestream.ChangeTypeModified)
-		require.NoError(t, err)
-
-		proposedChange := changestream.NewNodeChange(changestream.ChangeTypeModified, nodeID, graph.Kinds{}, props)
-
-		status, err := daemon.ResolveNodeChangeStatus(ctx, proposedChange)
-		require.NoError(t, err)
-		require.True(t, status.Changed)
-		require.True(t, status.Exists)
-	})
-
-	t.Run("cache miss, changelog enabled, no DB match", func(t *testing.T) {
-		daemon, ctx, teardown := setupIntegrationTest(t, true)
-		defer teardown()
-
-		props := graph.NewProperties().SetAll(map[string]any{"foo": "bar", "a": 1})
-		nodeID := "nothing-matches-me :("
-
-		proposedChange := changestream.NewNodeChange(changestream.ChangeTypeModified, nodeID, graph.Kinds{}, props)
-
-		status, err := daemon.ResolveNodeChangeStatus(ctx, proposedChange)
-		require.NoError(t, err)
-		require.False(t, status.Changed)
-		require.False(t, status.Exists)
-	})
-}
-
-func TestFlushNodeChanges(t *testing.T) {
-	daemon, ctx, teardown := setupIntegrationTest(t, true)
-	defer teardown()
-
-	// Prepare one buffered NodeChange
-	props := graph.NewProperties().Set("foo", "bar")
-	node := &changestream.NodeChange{
-		NodeID:     "test-node-123",
-		Properties: props,
-		Kinds:      graph.Kinds{graph.StringKind("NodeKind1")},
-	}
-	hash, _ := node.Properties.Hash(nil)
-	now := time.Now()
-
-	// queue a single change
-	daemon.QueueChange(ctx, now, node)
-
-	// Call flush
-	err := daemon.FlushNodeChanges(ctx)
-	require.NoError(t, err)
-
-	// Query the DB to verify the row exists
-	var (
-		nodeID         string
-		changeType     int
-		kindIDs        []int32
-		properties     []string
-		propertiesHash []byte
-	)
-
-	row := daemon.PGX().QueryRow(ctx, `
-		SELECT node_id, change_type, kind_ids, property_fields, hash
-		FROM node_change_stream
-		WHERE node_id = $1
-		ORDER BY created_at DESC
-		LIMIT 1
-	`, node.NodeID)
-
-	err = row.Scan(&nodeID, &changeType, &kindIDs, &properties, &propertiesHash)
-	require.NoError(t, err)
-
-	require.Equal(t, node.NodeID, nodeID)
-	require.Equal(t, changestream.ChangeTypeModified, changestream.ChangeType(changeType))
-	require.Equal(t, node.Properties.Keys(nil), properties)
-	require.Equal(t, hash, propertiesHash)
-}
 
 func TestChangelog(t *testing.T) {
 	t.Run("feature flag is off. always submit change. ", func(t *testing.T) {
-		changelog, ctx, teardown := setupIntegrationTestRefactor(t, false)
+		changelog, ctx, teardown := setupIntegrationTest(t, false)
 		defer teardown()
 
 		change := changestream.NewNodeChange(
@@ -325,7 +114,7 @@ func TestChangelog(t *testing.T) {
 		}
 	})
 	t.Run("node unvisited. submit the change.", func(t *testing.T) {
-		changelog, ctx, teardown := setupIntegrationTestRefactor(t, true)
+		changelog, ctx, teardown := setupIntegrationTest(t, true)
 		defer teardown()
 
 		change := changestream.NewNodeChange(
@@ -376,7 +165,7 @@ func TestChangelog(t *testing.T) {
 	})
 
 	t.Run("node visited. unchanged. skip submission.", func(t *testing.T) {
-		changelog, ctx, teardown := setupIntegrationTestRefactor(t, true)
+		changelog, ctx, teardown := setupIntegrationTest(t, true)
 		defer teardown()
 
 		change := changestream.NewNodeChange(
@@ -406,7 +195,7 @@ func TestChangelog(t *testing.T) {
 	})
 
 	t.Run("node visited. properties changed. submit the change.", func(t *testing.T) {
-		changelog, ctx, teardown := setupIntegrationTestRefactor(t, true)
+		changelog, ctx, teardown := setupIntegrationTest(t, true)
 		defer teardown()
 
 		change := changestream.NewNodeChange(
@@ -465,7 +254,7 @@ func TestChangelog(t *testing.T) {
 
 	// TODO: wire up kind changes
 	t.Run("node visited. kinds changed. submit the change.", func(t *testing.T) {
-		changelog, ctx, teardown := setupIntegrationTestRefactor(t, true)
+		changelog, ctx, teardown := setupIntegrationTest(t, true)
 		defer teardown()
 
 		change := changestream.NewNodeChange(
