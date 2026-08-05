de9e6c8dcb6180dea92fd873a98df35e40f33eb1
Reuben Lifshay <rlifshay@specterops.io>
2026-08-05 14:39:13 -0700

    fix: escape cypher debug statement with comments across newlines BED-9065 (#111)
    * fix: escape cypher debug statement with comments across newlines
    
    * test: add test to verify cypher debug statement format
    
    * test: improve cypher debug statement format test
    
    Resolves BED-9065

--- cypher/models/pgsql/translate/format.go
+++ cypher/models/pgsql/translate/format.go
@@ -3,6 +3,7 @@ package translate
 import (
 	"bytes"
 	"context"
+	"strings"
 
 	"github.com/specterops/dawgs/cypher/models/cypher"
 	cypherFormat "github.com/specterops/dawgs/cypher/models/cypher/format"
@@ -14,17 +15,39 @@ func Translated(translation Result) (string, error) {
 	return format.Statement(translation.Statement, format.NewOutputBuilder())
 }
 
+// postgres comments can be terminated by \r, \n, or both per the source:
+// https://github.com/postgres/postgres/blob/824d5f6241ea7a0a85c9d2b3d27beb78e42a36ab/src/backend/parser/scan.l#L186-L211
+var newlineToCommentReplacer = strings.NewReplacer(
+	"\r\n", "\n-- ",
+	"\r", "\n-- ",
+	"\n", "\n-- ",
+)
+
 func FromCypher(ctx context.Context, regularQuery *cypher.RegularQuery, kindMapper pgsql.KindMapper, stripLiterals bool, graphID int32) (format.Formatted, error) {
 	var (
 		output  = &bytes.Buffer{}
 		emitter = cypherFormat.NewCypherEmitter(stripLiterals)
 	)
 
-	output.WriteString("-- ")
+	// 1. write cypher to output
 
 	if err := emitter.Write(regularQuery, output); err != nil {
 		return format.Formatted{}, err
 	}
+
+	// 2. save copy of cypher and reset output for commented cypher
+
+	raw := strings.TrimSpace(output.String())
+	output.Reset()
+
+	// 3. write commented cypher
+
+	output.WriteString("-- ") // opening comment
+	if _, err := newlineToCommentReplacer.WriteString(output, raw); err != nil {
+		return format.Formatted{}, err
+	}
+
+	// 4. continue with SQL
 
 	output.WriteString("\n")
 
