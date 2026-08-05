2a4a843350acfbeb6cb43bc915b9670dd15f9937
Reuben Lifshay <rlifshay@specterops.io>
2026-08-05 14:39:13 -0700

    fix: escape cypher debug statement with comments across newlines BED-9065 (#111)
    * fix: escape cypher debug statement with comments across newlines
    
    * test: add test to verify cypher debug statement format
    
    * test: improve cypher debug statement format test
    
    Resolves BED-9065

---
+++ cypher/models/pgsql/translate/format_test.go
@@ -0,0 +1,41 @@
+package translate
+
+import (
+	"context"
+	"strings"
+	"testing"
+
+	"github.com/specterops/dawgs/cypher/frontend"
+	"github.com/specterops/dawgs/drivers/pg/pgutil"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFromCypherProperlyEscapesDebugComment(t *testing.T) {
+	t.Parallel()
+
+	kindMapper := pgutil.NewInMemoryKindMapper()
+
+	query, err := frontend.ParseCypher(
+		frontend.NewContext(),
+		"MATCH (n) WHERE n.`begin\nfail1\rfail2\r\nfail3\n\rfail4` = 1 RETURN n",
+	)
+	require.NoError(t, err)
+
+	formatted, err := FromCypher(context.Background(), query, kindMapper, false, DefaultGraphID)
+	require.NoError(t, err)
+
+	IsPGNewline := func(r rune) bool {
+		return r == '\n' || r == '\r'
+	}
+	for line := range strings.FieldsFuncSeq(formatted.Statement, IsPGNewline) {
+		if strings.HasPrefix(line, "with s0") {
+			break
+		}
+		is_commented := strings.HasPrefix(strings.TrimSpace(line), "--")
+		require.True(t, is_commented, "cypher line '%v' does not start with '--'", line)
+		if is_commented {
+			continue
+		}
+		require.NotContains(t, line, "fail")
+	}
+}
